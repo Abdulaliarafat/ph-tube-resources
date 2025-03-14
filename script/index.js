b41d58129170aff90b37e4e407a9e236f8ac46ac
abdulaliarafat <abdulaliarafat@gmail.com>
2025-03-14 23:14:47 +0600

    pero tube project cpmplete

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
+const showLoader=()=>{
+  document.getElementById('loader').classList.remove('hidden')
+  document.getElementById('video-container').classList.add('hidden')
+}
+const hiddenoader=()=>{
+  document.getElementById('loader').classList.add('hidden')
+  document.getElementById('video-container').classList.remove('hidden')
+}
 function removeActiveClass(){
   const activeButton=document.getElementsByClassName('active')
   for(let btn of activeButton){
@@ -11,8 +19,9 @@ function loadCatagories(){
     .then(data=>displayCatagories(data.categories))
 }
 // fetch 2
-function loadVideo(){
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+function loadVideo(searchtext =""){
+  showLoader()
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchtext}`)
     .then(res=>res.json())
     .then(data=>{
       removeActiveClass()
@@ -22,6 +31,7 @@ function loadVideo(){
 } 
 // fetch 3        
 const loadCatagoriesVideos=(id)=>{
+  showLoader()
   const url=`https://openapi.programming-hero.com/api/phero-tube/category/${id}`
   fetch(url)
   .then(res=>res.json())
@@ -100,6 +110,7 @@ const displayVideos=(videos)=>{
             <h2 class="font-bold text-4xl">Oops!! Sorry, There is no <br> content here</h2>
         </div>
       `;
+      hiddenoader()
       return
     }
     videos.forEach(video=>{
@@ -109,7 +120,7 @@ const displayVideos=(videos)=>{
             <figureb class="relative">
               <img class="w-full h-[200px] rounded-lg object-cover" src="${video.thumbnail}"
                 alt="Shoes" />
-                <span class="absolute left-46 bottom-30 text-white bg-black px-2 rounded-sm text-sm">3hrs 56 min ago</span>
+                <span class="absolute left-46 bottom-38 text-white bg-black px-2 rounded-sm text-sm">3hrs 56 min ago</span>
             </figure>
             <div class="flex gap-3 py-5 px-0">
             <div class="profile">
@@ -121,15 +132,21 @@ const displayVideos=(videos)=>{
             </div>
             <div class="intro">
                <h2 class="text-sm font-semibold">Midnight Serenade</h2>
-               <p class="text-sm text-gray-400 flex gap-1 ">${video.authors[0].profile_name} <img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></p>
+               <p class="text-sm text-gray-400 flex gap-1 ">${video.authors[0].profile_name}
+               ${video.authors[0].verified=== true? `<img class="w-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="">` : ``} </p>
                <p class="text-sm text-gray-400">${video.others.views}</p>
             </div>
             </div>
-            <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block hover:bg-red-500 hover:text-white rounded-md">Show Details</button>
+            <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block hover:bg-red-500 hover:text-white">Show Details</button>
           </div>
         `
         videoContainer.append(videoCard)
     })
+    hiddenoader()
 }
+document.getElementById('search-input').addEventListener('keyup',(e)=>{
+  const input=e.target.value;
+ loadVideo(input)
+})
 loadCatagories()
 
